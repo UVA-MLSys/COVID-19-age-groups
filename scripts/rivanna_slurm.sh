e5459c8afd1b765e37722c9e9a25420ba16de4ba
khairulislam <khairulislamtanim@gmail.com>
2023-11-20 10:52:33 -0500

    add crossformer results

--- scripts/rivanna_slurm.sh
+++ scripts/rivanna_slurm.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
-#SBATCH --job-name="interpret_without_ground_truth_test"
-#SBATCH --output=scripts/outputs/interpret_without_ground_truth_test.out
+#SBATCH --job-name="Crossformer_train_total"
+#SBATCH --output=scripts/outputs/Crossformer_train_total.out
 #SBATCH --partition=gpu
-#SBATCH --time=10:00:00  # can be reduced to 1h for Top 20 ot 500 counties
+#SBATCH --time=12:00:00  # can be reduced to 1h for Top 20 ot 500 counties
 #SBATCH --account=bii_dsc_community
 #SBATCH --gres=gpu:v100:1
 #SBATCH --mem=32GB # reduce to 16GB for Top 20 ot 500 counties
 # # SBATCH --mail-type=end
 # # SBATCH --mail-user=mi3se@virginia.edu
 
+
 source /etc/profile.d/modules.sh
 source ~/.bashrc
+
+# module load cuda cudnn
 
 # 1. using singularity
 module load singularity
 # outputing to scrach folder ensures temporary experiments aren't tracked
 # since scratch is added in gitignore
-# singularity run --nv timeseries.sif python run.py --data_path Top_20.csv --model FEDformer --disable_progress
-singularity run --nv timeseries.sif python interpret_without_ground_truth.py \
-    --data_path Total.csv --model FEDformer \
-    --explainers gradient_shap integrated_gradients deep_lift lime feature_ablation occlusion augmented_occlusion feature_permutation morris_sensitivity \
-    --disable_progress --flag test
+singularity run --nv timeseries.sif python run.py --data_path Total.csv --model Crossformer
+# singularity run --nv timeseries.sif python interpret_without_ground_truth.py \
+#     --data_path Total.csv --model FEDformer \
+#     --explainers feature_ablation \
+#     --disable_progress --flag updated --batch_size 64
 
 # # 2. using anaconda
-# module load cuda cudnn
 # module load anaconda
 # conda deactivate 
 # conda activate ml # replace with your own virtual env name
